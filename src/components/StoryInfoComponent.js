1d151f808f2b562bbf5279298d0d70935084e899
vit <vitaliidanylov1992@gmail.com>
2017-12-19 00:03:14 +0100

    added dialog window to storyInfoComponent

--- src/components/StoryInfoComponent.js
+++ src/components/StoryInfoComponent.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import { Button, Dialog, DialogContent, DialogActions } from 'react-mdl'
 
 export default class StoryInfoComponent extends Component {
+
+  state = {
+    openDialog: false
+  }
+
+  handleOpenDialog = () => {
+    this.setState({
+      openDialog: true
+    });
+  }
+
+  handleCloseDialog = () => {
+    this.setState({
+      openDialog: false
+    });
+  }
 
   render() {
     
@@ -9,12 +26,29 @@ export default class StoryInfoComponent extends Component {
       closeShowInfo 
     } = this.props;
 
+    const { openDialog } = this.state;
+
     return (
       <div>
+        <Dialog open={openDialog}>
+          <DialogContent>
+            lorem50
+          </DialogContent>
+          <DialogActions>          
+            <Button 
+              type='button' 
+              onClick={this.handleCloseDialog}
+            >
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
         <h2>Additional story info</h2>
         <p><b>Autor:</b> {story.by}</p>
         <p><b>Title:</b> {story.title}</p>
-        <p><b>Title:</b> {story.type}</p>        
+        <p><b>Title:</b> {story.type}</p>
+        <Button colored onClick={this.handleOpenDialog}>Read</Button>
+        <Button colored onClick={closeShowInfo}>Close</Button>
       </div>
     )
   }
