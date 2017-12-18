90e43f82eb446d84b0f7504dc6e48682cf905807
vit <vitaliidanylov1992@gmail.com>
2017-12-18 23:24:23 +0100

    added axios + added store to application

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import logo from '../styles/logo.svg';
 import '../styles/App.css';
+import { Provider } from 'react-redux'
+import store from '../redux/store/store'
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+// import StoriesContainer from './components/StoriesContainer'
+import { Grid, Cell } from 'react-mdl'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <div style={{width: '80%', margin: 'auto'}}>
+          <ReactHeader />
+        </div>
+      </Provider>    
     );
   }
 }
+
+const ReactHeader = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Top stories on HackerNews</h1>
+      </header>        
+    </div>
+  );
+};
 
 export default App;
